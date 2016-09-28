332d7390c02f52deef6e519b2f9304f68f52298f
Jospeh Kalb <kalb.joseph@gmail.com>
2016-09-28 16:16:10 -0700

    second commit

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
+var isLat = 32.715;
+var isLng = -117.164;
+
+var map;
+
+// Making the map 
+function initMap() {
+        // Create a map object and specify the DOM element for display.
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: isLat, lng: isLng},
+          scrollwheel: true,
+          zoom: 11
+});   	
+        // makes the markers!!!
+		$.getJSON("topspots.json", function(things){
+			$.each(things.topspots, function(index, items){
+        		var marker = new google.maps.Marker({
+      			position: {
+        			lat: items.location[0],
+        			lng: items.location[1]
+      			},
+      		map: map
+    	});
+		marker.setMap(map);
+});
+});
+}
+
 // 0_0 it works.... 
-$.getJSON("topspots.json", function(stuff){
-	$.each(stuff.topspots, function(index, items) {
-		$('table').append($('<tr><td>' + stuff.topspots[index].name 
-			+ '</td><td>'+ stuff.topspots[index].description 
-			+ '</td><td><a href=http://www.google.com/maps/place/'+ stuff.topspots[index].location[0]
-			+ ',' + stuff.topspots[index].location[1]
-			+'><button id='+ index 
-			+'>Open in Google Maps</button></a></td></tr>'));
-	
+$.getJSON("topspots.json", function(stuff){ // Grab the json file
+	$.each(stuff.topspots, function(index, items) { // sort through the topspots
+		$('table').append($('<tr><td>' + items.name // append the name
+			+ '</td><td>'+ items.description // append the description
+			+ '</td><td><a href=http://www.google.com/maps/place/'+ items.location[0] 
+			+ ',' + items.location[1] // append the link with the button
+			+'><button type="button" class="btn btn-info" id='+ index // give each button an id number
+			+'>Location</button></a></td></tr>'));
 	});
 });
